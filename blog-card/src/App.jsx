50e9486778890b27600628068535eca9a5030af8
Catur setyono <catursetyono542@gmail.com>
2024-09-06 00:06:52 +0700

    First Commit

---
+++ blog-card/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const App = () => {
+  return (
+    <div className="flex bg-yellow-300 text-red-300 w-full h-screen justify-center items-center">
+      <div className="card w-[384px] h-[522px] bg-white drop-shadow-md"></div>
+    </div>
+  )
+}
+
+export default App;
